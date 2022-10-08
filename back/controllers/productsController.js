5b28c29bcc6080a2690f6baf08820b72ffa2abef
emartinezb <estefa0223@gmail.com>
2022-10-07 20:08:49 -0500

    Tercer commit
    configuración base de datos

---
+++ back/controllers/productsController.js
@@ -0,0 +1,7 @@
+//Primer GetMapping con JS
+exports.getProducts=(req,res,next) =>{
+    res.status(200).json({
+        success:true,
+        message: "En esta ruta ud va a poder ver todos los productos"
+    })
+    }
