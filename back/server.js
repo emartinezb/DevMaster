ba888cf1be9e638f98c8721013e897dac2ca941c
emartinezb <estefa0223@gmail.com>
2022-10-07 21:46:29 -0500

    Base de Datos
    Conexión con la base de datos

--- back/server.js
+++ back/server.js
@@ -1,8 +1,13 @@
 const app=require("./app");
+const connectDatabase = require("./config/database");
 
 //Settear el archivo de configuración
 const dotenv=require("dotenv");
+const { connect } = require("mongoose");
 dotenv.config({path: 'back/config/config.env'})
+
+//configurar base de datos
+connectDatabase();
 
 //Llamo al server
 const server=app.listen(process.env.PORT, () => {
