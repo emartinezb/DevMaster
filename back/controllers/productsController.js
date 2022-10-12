5750e463d2cefe6cb4492803cb61655c82104bf1
emartinezb <estefa0223@gmail.com>
2022-10-11 20:16:51 -0500

    Clase 5: modelo y crud
    Creación del Esquema de Producto, y CRUD

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,7 +1,18 @@
-//Primer GetMapping con JS
-exports.getProducts=(req,res,next) =>{
+const producto = require("../models/productos")
+
+//Ver la lista de productos
+exports.getProducts=(req, res, next) => {
     res.status(200).json({
         success:true,
         message: "En esta ruta ud va a poder ver todos los productos"
     })
-    }
+}
+
+//Crear nuevo producto /api/productos
+exports.newProduct= async(req, res, next) => {
+    const product = await producto.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
+    })
+}
