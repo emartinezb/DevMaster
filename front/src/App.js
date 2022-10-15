6e730fffc51bf4e2532fe2e53063fc22913fb798
emartinezb <estefa0223@gmail.com>
2022-10-14 22:56:31 -0500

    Clase 7
    Inicio de front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de mascotas PetShop</h1>
+      <img src=".\images\petshop-logo.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
