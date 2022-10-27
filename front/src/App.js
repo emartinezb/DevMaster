1d144f597391ffe5d17225f031fa09a2163a58d7
emartinezb <estefa0223@gmail.com>
2022-10-27 17:34:02 -0500

    Actualización del front

--- front/src/App.js
+++ front/src/App.js
@@ -2,17 +2,26 @@ import './App.css';
 import React from 'react';
 import Header from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
+import Home from './components/Home';
+//Router de react
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
+    <Router> 
     <div className="App">
       <Header />
-      <center>Contenido aquí</center>
-      <center>Contenido aquí</center>
-      <center>Contenido aquí</center>
-      <center>Contenido aquí</center>
+      <div className='container container-fluid'>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Home" element={<Home />}></Route>
+        </Routes>
+
+      </div>
       <Footer/>
     </div>
+    </Router>
   );
 }
 
--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -3,7 +3,83 @@ import React, { Fragment } from 'react'
 export const Home = () => {
     return (
         <Fragment>
-            
+            <h1 id='encabezado_productos'>Últimos Productos</h1>
+
+            <section id='productos' className='container mt-5'>
+                <div className='row'>
+
+                    {/*Producto 1*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/masa_play_doh_tubo_10_latas.png' alt='Masa_play_doh_tubo_10_latas'></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id='titulo_producto'><a href='http://localhost:3000/'>Masa Moldeable Play-Doh Tubo de Fiesta X10 latas 280g (10 Oz)</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id='No_de_opiniones'>15 reviews</span>
+                                </div>
+                                <p className='card-text'>$19.900</p><a href='http://localhost:3000/' id='view_btn' className='btn btn-block'>Ver detalles</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 2*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/set_masa_kitchen_creations.png' alt='set_masa_kitchen_creations'></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id='titulo_producto'><a href='http://localhost:3000/'>Set de Masa Moldeable Play-Doh Kitchen Creations Batidora de Postres</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id='No_de_opiniones'>28 reviews</span>
+                                </div>
+                                <p className='card-text'>$99.900</p><a href='http://localhost:3000/' id='view_btn' className='btn btn-block'>Ver detalles</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 3*/}                    
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/minecraft_set_capitan_ravenger.png' alt='minecraft_set_capitan_ravenger'></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id='titulo_producto'><a href='http://localhost:3000/'>Set Capitan Raid y Ravanger - Minecraft</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id='No_de_opiniones'>53 reviews</span>
+                                </div>
+                                <p className='card-text'>$139.800</p><a href='http://localhost:3000/' id='view_btn' className='btn btn-block'>Ver detalles</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/*Producto 4*/}
+                    <div className='col-sm-12 col-md-6 col-lg-3 my-'>
+                        <div className='card p-3 rounded'>
+                            <img className='card-img-top mx-auto' src='./images/peluche_oso_pardo_sentado_100cm.png' alt='peluche_oso_pardo_sentado_100cm'></img>
+                            <div className='card-body d-flex flex-column'>
+                                <h5 id='titulo_producto'><a href='http://localhost:3000/'>Peluche Oso Pardo Sentado Kisses 100 cm</a></h5>
+                                <div className='rating mt-auto'>
+                                    <div className='rating-outer'>
+                                        <div className='rating-inner'></div>
+                                    </div>
+                                    <span id='No_de_opiniones'>46 reviews</span>
+                                </div>
+                                <p className='card-text'>$104.900</p><a href='http://localhost:3000/' id='view_btn' className='btn btn-block'>Ver detalles</a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </Fragment>
     )
 }
+
+export default Home;
